43c97bfba88dcbd65d32e80707acab0ec74d6776
Mu05 <naoya.ueno.mu@gmail.com>
2025-09-07 15:31:06 +0900

    chore: Update setup.bat

--- setup.bat
+++ setup.bat
@@ -2,12 +2,5 @@
 setlocal
 set "BASE=%~dp0"
 where pwsh >nul 2>&1 && (set "PS=pwsh") || (set "PS=powershell")
-
-"%PS%" -NoLogo -NoProfile -Command ^
-  "Set-ExecutionPolicy RemoteSigned -Scope Process -Force; ^
-   Set-Location -LiteralPath '%BASE%'; ^
-   Unblock-File -LiteralPath '.\setup.ps1' -ErrorAction SilentlyContinue; ^
-   & '.\setup.ps1'; exit $LASTEXITCODE"
-
+"%PS%" -NoLogo -NoProfile -Command "Set-ExecutionPolicy RemoteSigned -Scope Process -Force; Set-Location -LiteralPath '%BASE%'; Unblock-File -LiteralPath '.\setup.ps1' -ErrorAction SilentlyContinue; & '.\setup.ps1'; exit $LASTEXITCODE"
 exit /b %ERRORLEVEL%
-s
